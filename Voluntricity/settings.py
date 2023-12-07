8273cc90f4818674bffc6293d1a1fb1fb250eaa1
Nikhil Singhal <70971092+NikhilSinghal1704@users.noreply.github.com>
2023-12-07 11:21:08 +0530

    Update settings.py

--- Voluntricity/settings.py
+++ Voluntricity/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ybty!e!z17#+9ba(lu!op$znzd#8+(ugmgbjc=d-d4b+6jgvwi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
