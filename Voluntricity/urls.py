56e5f878c6b1738b7ab425b0e38698749ad222a0
Nikhil Singhal <70971092+NikhilSinghal1704@users.noreply.github.com>
2023-12-07 17:42:07 +0530

    user profile completed

--- Voluntricity/urls.py
+++ Voluntricity/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('', include('Home.urls')),
@@ -25,3 +27,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
