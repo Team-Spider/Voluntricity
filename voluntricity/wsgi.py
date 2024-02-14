754b54d22d1a8931013da32f7ad269a5b059e2f7
Nikhil Singhal <70971092+NikhilSinghal1704@users.noreply.github.com>
2024-02-14 13:10:23 +0530

    Update wsgi.py

--- voluntricity/wsgi.py
+++ voluntricity/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# add your project directory to the sys.path
+project_home = '/home/voluntricity/voluntricity'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'voluntricity.settings'
+
+
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voluntricity.settings')
-
 application = get_wsgi_application()
