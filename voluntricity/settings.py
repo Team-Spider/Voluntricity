3f6411d5ad9a9789885f3a6254b22fd1393a56ae
Nikhil Singhal <70971092+NikhilSinghal1704@users.noreply.github.com>
2024-02-14 12:50:56 +0530

    Update settings.py

--- voluntricity/settings.py
+++ voluntricity/settings.py
@@ -33,10 +33,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-#CSRF_TRUSTED_ORIGINS = ["https://contributions-alt-cl-gr.trycloudflare.com"]
+CSRF_TRUSTED_ORIGINS = ["http://74.235.202.2:8000", "http://0.0.0.0:8000"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8000", "http://74.235.202.2:8000", "http://0.0.0.0:8000"
 ]
 
 
